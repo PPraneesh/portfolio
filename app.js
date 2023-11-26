16672d4de3fcc41c70cc248e295ac7a5ca20ec34
Praneesh <parshipraneesh8@gmail.com>
2023-11-26 11:43:51 +0530

    azureLogin

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ app.get('/resume',(req,res)=>{
     res.contentType("application/pdf");
     res.sendFile(__dirname+'/public/resume.pdf');
 });
-app.listen(3000, () => {
+app.listen(3000|| process.env.PORT, () => {
     console.log('Server is running on port 3000');
 });
